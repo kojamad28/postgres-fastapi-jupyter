ccb583b82e6a4479b10652ce4bff78ecc7a3694e
kojamad28 <kojamad28@gmail.com>
2024-12-31 19:33:38 +0900

    updated loaded env file for development

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,7 +2,8 @@ from dotenv import dotenv_values
 from sqlalchemy import URL
 from sqlmodel import SQLModel, Session, create_engine
 
-config = dotenv_values("db/.env")
+config = dotenv_values("db/.env.dev")
+#config = dotenv_values("db/.env")
 
 DEBUG = config.get("DEBUG", "false").lower() == "true"
 
