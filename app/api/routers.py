4506101d15663db0e4dcf6bf54bde78217507dd3
kojamad28 <kojamad28@gmail.com>
2024-12-31 17:11:26 +0900

    updated routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -7,26 +7,26 @@ from .dependencies import get_session
 
 router = APIRouter()
 
-@router.get('/users/', response_model=list[models.RetrievedUser])
+@router.get('/list/', response_model=list[models.RetrievedUser])
 def get_users(session: Session = Depends(get_session), offset: int = Query(default=0), limit: int = Query(default=100, lte=100)):
     return crud.retrieve_models(session, models.User, offset, limit)
 
 
-@router.get('/user/{target}', response_model=models.RetrievedUser)
+@router.get('/{target}', response_model=models.RetrievedUser)
 def get_user(target: str, session: Session = Depends(get_session)):
     return crud.retrieve_model(session, models.User, 'name', target)
 
 
-@router.post('/users/', response_model=models.RetrievedUser)
+@router.post('/list/', response_model=models.RetrievedUser)
 def create_user(created_user: models.CreatedUser, session: Session = Depends(get_session)):
     return crud.create_model(session, models.User, created_user)
 
 
-@router.put('/user/{target}', response_model=models.RetrievedUser)
+@router.put('/{target}', response_model=models.RetrievedUser)
 def update_user(target: str, updated_user: models.UpdatedUser, session: Session = Depends(get_session)):
     return crud.update_model(session, models.User, 'name', target, updated_user)
 
 
-@router.delete('/user/{target}')
+@router.delete('/{target}')
 def delete_user(target: str, session: Session = Depends(get_session)):
     return crud.delete_model(session, models.User, 'name', target)
--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ from api.dependencies import create_db_and_tables
 
 app = FastAPI()
 
-app.include_router(routers.router)
+app.include_router(
+    routers.router,
+    prefix="/api/user",
+    tags=["user"]
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
