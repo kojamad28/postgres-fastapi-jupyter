13c52599dfdad5b0de471154c55b21a16642bdc6
kojamad28 <kojamad28@gmail.com>
2025-01-03 20:02:59 +0900

    updated routings and type hints

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -11,34 +11,34 @@ from api.schemas.user import RetrievedUser, CreatedUser, UpdatedUser
 router = APIRouter()
 
 
-@router.get('/users/', response_model=List[RetrievedUser])
+@router.get('/list/', response_model=List[RetrievedUser])
 async def get_users(
     session: Session = Depends(get_session), offset: int = Query(default=0), limit: int = Query(default=100, lte=100)
-) -> RetrievedUser:
+) -> List[RetrievedUser]:
     return retrieve_models(session, User, offset, limit)
 
 
-@router.get('/user/{target}', response_model=RetrievedUser)
+@router.get('/{target}', response_model=RetrievedUser)
 async def get_user(
     target: str, session: Session = Depends(get_session)
 ) -> RetrievedUser:
     return retrieve_model(session, User, 'name', target)
 
 
-@router.post('/users/', response_model=RetrievedUser)
+@router.post('/list/', response_model=RetrievedUser)
 async def create_user(
     created_user: CreatedUser, session: Session = Depends(get_session)
 ) -> RetrievedUser:
     return create_model(session, User, created_user)
 
 
-@router.put('/user/{target}', response_model=RetrievedUser)
+@router.put('/{target}', response_model=RetrievedUser)
 async def update_user(
     target: str, updated_user: UpdatedUser, session: Session = Depends(get_session)
 ) -> RetrievedUser:
     return update_model(session, User, 'name', target, updated_user)
 
 
-@router.delete('/user/{target}')
+@router.delete('/{target}')
 async def delete_user(target: str, session: Session = Depends(get_session)) -> dict:
     return delete_model(session, User, 'name', target)
