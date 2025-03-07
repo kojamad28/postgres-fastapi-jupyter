0b5696c23b13bea2c2d05360b7e62a37f52f9167
kojamad28 <kojamad28@gmail.com>
2025-03-07 19:16:50 +0900

    added basic codes

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from api.dependencies import create_db_and_tables
-from api.routers import user
+from api.dependencies import create_db_and_tables, DEBUG
+from api.routers import base
 
-app = FastAPI()
-
-app.include_router(
-    user.router,
-    prefix="/api/user",
-    tags=["user"]
-)
+app = FastAPI(debug=DEBUG)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
+
+if DEBUG:
+    app.include_router(
+        base.router_dev,
+        prefix="/api/user",
+        tags=["Base"]
+    )
+else:
+    app.include_router(
+        base.router,
+        prefix="/api/user",
+        tags=["Base"]
+    )
 
 
 @app.on_event('startup')
