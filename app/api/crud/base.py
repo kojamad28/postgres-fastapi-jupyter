6ee2bb8b3361ee279781f696af4a4961ebcb5004
kojamad28 <kojamad28@gmail.com>
2025-03-18 09:19:08 +0900

    updated to sqlalchemy codes

--- app/api/crud/base.py
+++ app/api/crud/base.py
@@ -1,5 +1,24 @@
 from fastapi import HTTPException
-from sqlmodel import SQLModel, Session, select
+from sqlalchemy import text, select
+from sqlalchemy.orm import Session
+from sqlmodel import SQLModel
+
+
+def check_connection(session: Session) -> dict:
+    try:
+        session.execute(text("SELECT 1"))
+        return {
+            "Status": "Success",
+            "Detail": "Connection to database is successful",
+            "Host": session.bind.url.host,
+            "Database": session.bind.url.database,
+            "Query": session.bind.url.query,
+        }
+    except Exception as e:
+        return {
+            "Status": "Failed",
+            "Detail": str(e),
+        }
 
 
 def create_model(session: Session, model: type[SQLModel], created_model: SQLModel):
@@ -11,19 +30,19 @@ def create_model(session: Session, model: type[SQLModel], created_model: SQLMode
 
 
 def retrieve_models(session: Session, model: type[SQLModel], offset: int, limit: int):
-    models = session.exec(select(model).offset(offset).limit(limit)).all()
+    models = session.execute(select(model).offset(offset).limit(limit)).all()
     return models
 
 
 def retrieve_model(session: Session, model: type[SQLModel], index_col: str, target: str):
-    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    db_model = session.execute(select(model).where(getattr(model, index_col) == target)).first()
     if not db_model:
         raise HTTPException(status_code=404, detail=f'{target} not found')
     return db_model
 
 
 def update_model(session: Session, model: type[SQLModel], index_col: str, target: str, updated_model: SQLModel):
-    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    db_model = session.execute(select(model).where(getattr(model, index_col) == target)).first()
     if not db_model:
         raise HTTPException(status_code=404, detail=f'{target} not found')
     updated_model_data = updated_model.dict(exclude_unset=True)
@@ -36,7 +55,7 @@ def update_model(session: Session, model: type[SQLModel], index_col: str, target
 
 
 def delete_model(session: Session, model: type[SQLModel], index_col: str, target: str):
-    db_model = session.exec(select(model).where(getattr(model, index_col) == target)).first()
+    db_model = session.execute(select(model).where(getattr(model, index_col) == target)).first()
     if not db_model:
         raise HTTPException(status_code=404, detail=f'{target} not found')
     session.delete(db_model)
